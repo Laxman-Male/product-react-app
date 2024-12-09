8d56cbae4f5d2e60acea006fd9a0b2a4975d6c04
Laxman-Male <laxmanmale122@gmail.com>
2024-12-09 23:13:11 +0530

    Deploy build to GitHub Pages

---
+++ src/Components/About.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+<div className='flex justify-center'>
+        <p className='text-4xl font-semibold p-4'> why to choose us ? </p>
+
+</div>
+        <div className=' flex justify-center'>
+
+       
+<section className='grid lg:grid-cols-3 xl:grid-cols-3      rounded-lg  shadow-2xl  md:grid-cols-3 sm:grid-cols-2 ssm:grid-cols-1  gap-5    '>
+ 
+        
+   <div className='p-3'>
+    <img className='mix-blend-multiply w-full h-36 rounded-3xl' src='https://www.victorybailbonds.com/wp-content/uploads/2016/04/affordable.jpg'/>
+    
+    <div className='  p-2'>
+        <p className='font-semibold'> Discover top-quality, </p>
+        <p className='font-semibold'> affordable headphones   </p>
+        <p className='font-semibold'> that delivers   </p>
+        <p className='font-semibold'> exceptional sound</p>
+        <p className='font-semibold'>without  breaking </p>
+        <p className='font-bold'> the bank.</p>
+    </div>
+   </div>
+
+
+   <div>
+    <img className='mix-blend-multiply w-full h-40 rounded-3xl' src='https://thumbs.dreamstime.com/b/product-durability-concept-sign-illustration-dark-green-gear-wheel-figures-pale-grdient-162365359.jpg'/>
+    
+    <div className='  p-2 ' >
+        <p className='font-semibold '> Built to last, </p>
+        <p className='font-semibold '>  our products are designed  </p>
+        <p className='font-semibold '>with durable materials </p>
+        <p className='font-semibold '> for long-term use and </p>
+        <p className='font-bold' >reliability.</p>
+    </div>
+   </div>
+
+
+   <div>
+    <img className='mix-blend-multiply w-full h-40 rounded-3xl' src='https://tse4.mm.bing.net/th/id/OIP.nr-8OL4NgtPv7sHE6rDFYgHaEd?rs=1&pid=ImgDetMain'/>
+    
+    <div className='  p-2 '>
+        <p className='font-semibold'> Shop with confidence </p>
+        <p className='font-semibold'> on our trusted website,  </p>
+        <p className='font-semibold'> where security and customer  </p>
+        <p className='font-semibold'>satisfaction are our top </p>
+        <p className='font-semibold'>priorities.</p>
+    </div>
+   </div>
+ 
+
+   
+    
+</section>
+</div>
+
+  
+        </>
+    )
+}
+
+export default About
---
+++ src/Components/Fotter.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Fotter = () => {
+    return (
+        <div className=' bg-violet-900  grid lg:grid-cols-3 xl:grid-cols-3      rounded-md    md:grid-cols-2 sm:grid-cols-2 ssm:grid-cols-1  gap-4 p-10 mt-10 '>
+            <div className='  p-4 flex flex-col items-center'>
+                <p className='font-bold text-2xl text-violet-200'> India mart </p>
+                <p className='font-sans text-lg text-violet-50'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique aliquid ad qui porro nam doloribus maiores commodi in vitio!ae distinct</p>
+            </div>
+
+           
+            <div className=' -500 p-4 sm: items-center flex flex-col'>
+                <p className='text-blue-300 font-semibold font-sans text-xl'> Quick Link </p>
+                <p className='hover:cursor-pointer hover:text-violet-50'>FAQ's </p>
+                <p className='hover:cursor-pointer hover:text-violet-50'>Cookies Policy </p>
+                <p className='hover:cursor-pointer hover:text-violet-50'> Terms and Condition </p>
+                <p className='hover:cursor-pointer hover:text-violet-50'> Customer Support  </p>
+                <p className='hover:cursor-pointer hover:text-violet-50'> @Copyrights </p>
+            </div>
+
+            {/* <div className='border border-red-500 p-4      flex flex-col justify-center items-center'> */}
+            <div className='  p-4 sm:flex sm:flex-col sm:justify-center  sm:items-center md:col-span-2 sm:col-span-2 lg:col-span-1 xl:col-span-1'>
+                <p className='text-lg text-violet-50 m-3 font-semibold '> Subscribe To Newsletters </p>
+                <form>
+                    <div>
+
+                    <input type='email' placeholder='Your email-here' className='w-48 rounded-md  h-8 p-2' />
+                    </div>
+                    <button className='border border-violet-50 w-48 rounded-md p-1 mt-3   font-semibold  bg-violet-200 text-violet-900 hover:bg-violet-700 hover:text-violet-50'> Subscribe </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Fotter
