23d579bc1e87616e3d2ae06bb4dbd599258cea4f
Laxman-Male <laxmanmale122@gmail.com>
2024-12-17 12:47:39 +0530

    setting ele not working

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,6 +7,12 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { faUserNurse } from '@fortawesome/free-solid-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+// import { faFacebook } from '@fortawesome/free-solid-svg-icons';
+// import { faLinkedin } from '@fortawesome/free-solid-svg-icons';
+// import { faYoutube } from '@fortawesome/free-solid-svg-icons';
+// import { faInstagram } from '@fortawesome/free-solid-svg-icons';
+
 
 import { Offcanvas, Button, Dropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,7 +21,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useForm, Controller } from "react-hook-form"
 // import { Input } from "@material-ui/core"
 
- 
+
 
 
 
@@ -27,12 +33,12 @@ const Navbar = () => {
 
     const { control, handleSubmit } = useForm({
         defaultValues: {
-          firstName: "",
-          select: {},
+            firstName: "",
+            select: {},
         },
-      })
-      const onSubmit = (data) => console.log(data)
-    
+    })
+    const onSubmit = (data) => console.log(data)
+
 
 
 
@@ -52,52 +58,69 @@ const Navbar = () => {
                         {/* <button className='border border-black flex justify-start mt-2'> 
                             <img src='img/setting.png' className='w-6 mix-blend-darken'/>
                              </button> */}
-                        <div className=' mt-2'>
+                        <div className=' mt-2 '>
                             <Button variant='outline-secondary' onClick={handleShow}>
                                 <img src='img/setting.png' className='w-6 mix-blend-darken ssm:w-5  sm:w-5 md:w-8 lg:8  ' />
                             </Button>
+                           
 
-                            <Offcanvas show={show} onHide={handleClose} placement="start">
+                            <Offcanvas  show={show} onHide={handleClose} placement="start">
                                 <Offcanvas.Header closeButton>
                                     <Offcanvas.Title >Setting </Offcanvas.Title>
                                 </Offcanvas.Header>
-                                <Offcanvas.Body>
-                                    <div>
-                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam, repudiandae?
+                                <Offcanvas.Body >
+                                    <div className='p-1 cursor-pointer'>
+                                        <FontAwesomeIcon icon={faUser} />     View your Profile
                                     </div>
-                                    <Dropdown className="mt-3 ">
-                                        <Dropdown.Toggle variant="primary" id="dropdown-basic"  >
-                                            Languages
-                                        </Dropdown.Toggle>
 
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item href="#">Action</Dropdown.Item>
-                                            <Dropdown.Item href="#">Another action</Dropdown.Item>
-                                            <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                                        </Dropdown.Menu>
-                                        
-                                    </Dropdown>
-                                    <form className='border border-black text-lg  '> 
-                                        <div> 
-                                        <input type='checkbox'/> 
-                                        <label classname="ml-5"> EarPhone</label>
+                                    <form className=' text-lg  p-2'>
+                                        <div>
+                                            <input type='checkbox'  />
+                                            <label className='px-2'> EarPhone</label>
                                         </div>
                                         <div>
-                                        <input type='checkbox'/> 
-                                        <label> HeadPhone</label>
+                                            <input type='checkbox' />
+                                            <label className='px-2'> HeadPhone</label>
                                         </div>
                                         <div>
-                                        <input type='checkbox'/> 
-                                        <label> SmartWatch  </label>
+                                            <input type='checkbox' />
+                                            <label className='px-2'> SmartWatch  </label>
                                         </div>
                                         <div>
-                                        <input type='checkbox'/> 
-                                        <label>Wireless </label>
+                                            <input type='checkbox' />
+                                            <label className='px-2'>Wireless </label>
                                         </div>
                                     </form>
                                 </Offcanvas.Body>
-                                
+                                <div>
+                                    <p className='font-semibold m-0 '> Contact us at </p>
+                                    <ul className='flex gap-4'>
+                                        <li>
+                                            <img className='w-5 cursor-pointer' src='https://tse1.mm.bing.net/th/id/OIP.Rwdx3-MJLtAa17YZIFMunAAAAA?rs=1&pid=ImgDetMain' />
+                                        </li>
+                                        <li>
+                                            <img className='w-5 cursor-pointer' src='https://image.similarpng.com/very-thumbnail/2020/06/Black-icon-Instagram-logo-transparent-PNG.png' />
+                                        </li>
+                                        <li>
+                                            <img className='w-5 cursor-pointer' src='https://th.bing.com/th/id/R.8f265a94a8fd9b0cb24488678ab6d648?rik=79r6hq6icjgjRg&riu=http%3a%2f%2flofrev.net%2fwp-content%2fphotos%2f2016%2f07%2fyoutube_black_logo.jpg&ehk=PyW73Rx8P9vyochnjVRFEEH0HO8s8Xzgt7XVUA3igrE%3d&risl=&pid=ImgRaw&r=0' />
+                                        </li>
+                                        <li>
+                                            <img className='w-5 cursor-pointer' src='https://tse3.mm.bing.net/th/id/OIP.DtzCFTem9bd_3GlVROBlRgHaHa?rs=1&pid=ImgDetMain' />
+                                        </li>
+                                    </ul>
+
+
+
+                                    {/* <FontAwesomeIcon icon={faFacebook} /> */}
+                                    {/* <FontAwesomeIcon icon={faLinkedin} />
+                                    
+                                    <FontAwesomeIcon icon={faYoutube} />
+                                    <FontAwesomeIcon icon={faInstagram} /> */}
+
+                                </div>
+
                             </Offcanvas>
+                                    
                         </div>
                     </div>
                     {/* <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
@@ -135,18 +158,18 @@ const Navbar = () => {
                     <ul className='flex gap-5 items-center px-5 py-2'>
 
                         <li className=' font-semibold hover:font-bold cursor-pointer text-xl'>
-                         
-                         <FontAwesomeIcon icon={faHouse} />
-                             </li>
-                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'> 
-                        <FontAwesomeIcon icon={faGlobe} />
-                             </li>
-                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'> 
-                        <FontAwesomeIcon icon={faUserNurse} />
-                             </li>
-                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'> 
-                        <FontAwesomeIcon icon={faPhone} />
-                             </li>
+
+                            <FontAwesomeIcon icon={faHouse} />
+                        </li>
+                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'>
+                            <FontAwesomeIcon icon={faGlobe} />
+                        </li>
+                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'>
+                            <FontAwesomeIcon icon={faUserNurse} />
+                        </li>
+                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'>
+                            <FontAwesomeIcon icon={faPhone} />
+                        </li>
                         {/* <buttonn onclick={handleCartDesign} > */}
 
                         {/* <li className='cursor-pointer' onClick={handleCartDesign}><lord-icon
