435bbd9f3d12680080928a71a3d6b21d486d10e6
Laxman-Male <laxmanmale122@gmail.com>
2025-01-02 23:22:57 +0530

    feat: making buy now btn as shop all btn

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,53 +1,71 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { use } from "react";
+import ShopList from "./ShopList";
+import Shopping from "./Shopping";
 
 const Home = () => {
+  const [Buynow, setBuynow] = useState(false);
+  const toggleShop = () => {
+    console.log("buy now ");
+    setBuynow(true);
+  };
+
   return (
     <>
       <div className=" flex justify-end  items-center  lg:flex-row  md:flex-col-reverse sm:flex-col-reverse   xs:flex-col-reverse ">
-        <div >
+        <div>
           <motion.p
-            className='text-6xl font-sans ssm:text-5xl ssm:font-sans moreSmall:ml-7 moreSmall:
-            mr-2 moreSmall:text-4xl '
-            initial={{ opacity: 0, y: 30 }} 
+            className="text-6xl font-sans ssm:text-5xl ssm:font-sans moreSmall:ml-7 moreSmall:
+            mr-2 moreSmall:text-4xl "
+            initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            >
-               New one! 
-            </motion.p> 
+          >
+            New one!
+          </motion.p>
 
           <motion.p
-           className='text-8xl font-bold ssm:text-6xl ssm:font-sans moreSmall:ml-7 moreSmall:mr-2 moreSmall:text-5xl '
-           initial={{ opacity: 0, y: 20 }} 
-           animate={{ opacity: 1, y: 0 }}
-           transition={{ duration: 1,delay:1 }}
-           > 
-           A3 HeadPhone 
-           </motion.p>
- 
+            className="text-8xl font-bold ssm:text-6xl ssm:font-sans moreSmall:ml-7 moreSmall:mr-2 moreSmall:text-5xl "
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
+            A3 HeadPhone
+          </motion.p>
 
           <motion.div
-           className='flex justify-around w-3/4   ssm:justify-between '
-           initial={{ opacity: 0, y: 40 }} 
-           animate={{ opacity: 1, y: 0 }}
-           transition={{ duration: 1, delay:0.5 }}
-           >
-
-            <button className='bg-violet-400  px-3 py-2 font-bold text-xl rounded-2xl text-white hover:bg-violet-700   cursor-pointer mt-10 moreSmall:ml-3 moreSmall:h-11 '>    Buy now    </button>
-            <button className='bg-violet-400  px-3 py-2 font-bold text-xl rounded-2xl text-white hover:bg-violet-700 cursor-pointer mt-10 moreSmall:h-11 ml-5 ' >    Shop all    </button>
+            className="flex justify-around w-3/4   ssm:justify-between "
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            <button
+              className="bg-violet-400  px-3 py-2 font-bold text-xl rounded-2xl text-white hover:bg-violet-700   cursor-pointer mt-10 moreSmall:ml-3 moreSmall:h-11 "
+              onClick={toggleShop}
+            >
+              Buy Now{" "}
+            </button>
+            <button className="bg-violet-400  px-3 py-2 font-bold text-xl rounded-2xl text-white hover:bg-violet-700 cursor-pointer mt-10 moreSmall:h-11 ml-5 ">
+              {" "}
+              Shop all{" "}
+            </button>
           </motion.div>
         </div>
 
-        <motion.img className='   mix-blend-darken' width={600} src='img/h3.jpg' alt='new product'
-            initial={{ opacity: 0, x: 20 }} 
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, delay: 1 }}
+        <motion.img
+          className="   mix-blend-darken"
+          width={600}
+          src="img/h3.jpg"
+          alt="new product"
+          initial={{ opacity: 0, x: 20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 1 }}
         />
-
       </div>
-
+      {/* <Shopping toggleShop={toggleShop} /> Buy now{" "} */}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/Components/Shopping.jsx
+++ src/Components/Shopping.jsx
@@ -1,145 +1,278 @@
-import React, { useState } from 'react'
-import ShopList from './ShopList'
-import Cart from './Cart'
+import React, { useState } from "react";
+import ShopList from "./ShopList";
+import Cart from "./Cart";
+import Home from "./Home";
 const Shopping = ({ addcart }) => {
+  const [shop, setshop] = useState(false);
+  const handleshop = () => {
+    setshop(!shop);
+    console.log(shop);
+  };
+  const [quantity, setquantity] = useState({});
 
-    const [shop, setshop] = useState(false)
-    const handleshop = () => {
-        setshop(!shop);
-        console.log(shop)
-    }
-    const [quantity, setquantity] = useState({});
+  const [removeItem, setremoveItem] = useState(false);
+  const [backToCart, setbackToCart] = useState(true);
+  const [cart, setcart] = useState([]);
+  const [filter, setFilter] = useState("All");
 
-    const [removeItem, setremoveItem] = useState(false)
-    const [backToCart, setbackToCart] = useState(true)
-    const [cart, setcart] = useState([])
-    const [filter,setFilter]= useState('All')
+  const AddtoCart = (product) => {
+    setcart((prevCart) => {
+      if (prevCart.some((item) => item.id === product.id)) {
+        return prevCart;
+      } else {
+        console.log("adding new product");
 
-    const AddtoCart = (product) => {
-        setcart(prevCart => {
-            if (prevCart.some(item => item.id === product.id)) {
-                 return prevCart
-            }
-            else {
-                console.log("adding new product")
+        setquantity((prev) => ({
+          ...prev,
+          [product.id]: 1,
+        }));
 
-                setquantity(prev => ({
-                    ...prev,
-                    [product.id]: 1
-                }));
+        return [...prevCart, product];
+      }
+    });
+  };
 
-                return [...prevCart, product];
-            }
-        })
-    };
+  const handleremove = (itemid) => {
+    setcart((prevCart) => prevCart.filter((item) => item.id !== itemid));
 
-    const handleremove = (itemid) => {
-        setcart((prevCart) => prevCart.filter(item => item.id !== itemid));
+    setquantity((prev) => {
+      const updatedQuantities = { ...prev };
 
-        setquantity(prev => {
-            const updatedQuantities = { ...prev };
+      delete updatedQuantities[itemid];
+      return updatedQuantities;
+    });
+  };
 
-            delete updatedQuantities[itemid];
-            return updatedQuantities;
-        });
+  const productData = [
+    {
+      id: "1",
+      title: "EarPhone",
+      review: "⭐⭐⭐⭐",
+      img: "img/e1.jpg",
+      price: "100",
+    },
+    {
+      id: "2",
+      title: "HeadPhone",
+      review: "⭐⭐⭐",
+      img: "img/h1.jpg",
+      price: "699",
+    },
+    {
+      id: "3",
+      title: "Wireless",
+      review: "⭐⭐⭐⭐",
+      img: "img/we1.jpg",
+      price: "599",
+    },
+    {
+      id: "4",
+      title: "SmartWatch",
+      review: "⭐⭐⭐⭐",
+      img: "img/ww1.jpeg",
+      price: "799",
+    },
+    {
+      id: "5",
+      title: "EarPhone",
+      review: "⭐⭐⭐⭐",
+      img: "img/e3.jpg",
+      price: "299",
+    },
 
-    }
+    {
+      id: "6",
+      title: "HeadPhone",
+      review: "⭐⭐⭐⭐",
+      img: "img/h3.jpg",
+      price: "899",
+    },
+    {
+      id: "7",
+      title: "Wireless",
+      review: "⭐⭐⭐",
+      img: "img/we3.jpg",
+      price: "699",
+    },
+    {
+      id: "8",
+      title: "SmartWatch",
+      review: "⭐⭐",
+      img: "img/ww3.png",
+      price: "1999",
+    },
+    {
+      id: "9",
+      title: "EarPhone",
+      review: "⭐⭐⭐",
+      img: "img/e4.jpg",
+      price: "199",
+    },
+    {
+      id: "10",
+      title: "HeadPhone",
+      review: "⭐⭐⭐⭐",
+      img: "img/h4.jpg",
+      price: "1099",
+    },
+    {
+      id: "11",
+      title: "Wireless",
+      review: "⭐⭐",
+      img: "img/we4.jpg",
+      price: "799",
+    },
+    {
+      id: "12",
+      title: "SmartWatch",
+      review: "⭐⭐⭐",
+      img: "img/ww4.jpg",
+      price: "1299",
+    },
+    {
+      id: "13",
+      title: "EarPhone",
+      review: "⭐⭐⭐",
+      img: "img/e5.jpg",
+      price: "329",
+    },
+    {
+      id: "14",
+      title: "HeadPhone",
+      review: "⭐⭐⭐⭐",
+      img: "img/h5.jpg",
+      price: "749",
+    },
+    {
+      id: "15",
+      title: "Wireless",
+      review: "⭐⭐⭐⭐",
+      img: "img/we5.jpg",
+      price: "659",
+    },
+    {
+      id: "16",
+      title: "SmartWatch",
+      review: "⭐⭐⭐⭐",
+      img: "img/ww5.jpg",
+      price: "999",
+    },
+  ];
 
-    const productData = [
-        { id: "1", title: "EarPhone", review: "⭐⭐⭐⭐", img: "img/e1.jpg", price: "100" },
-        { id: "2", title: "HeadPhone", review: "⭐⭐⭐", img: "img/h1.jpg", price: "699" },
-        { id: "3", title: "Wireless", review: "⭐⭐⭐⭐", img: "img/we1.jpg", price: "599" },
-        { id: "4", title: "SmartWatch", review: "⭐⭐⭐⭐", img: "img/ww1.jpeg", price: "799" },
-        { id: "5", title: "EarPhone", review: "⭐⭐⭐⭐", img: "img/e3.jpg", price: "299" },
+  const filterEarphone = (category) => {
+    setFilter(category);
+    console.log(filter);
+    console.log(productData);
+  };
+  const filteredData = productData.filter(
+    (product) => filter === "All" || product.title === filter
+  );
 
-        { id: "6", title: "HeadPhone", review: "⭐⭐⭐⭐", img: "img/h3.jpg", price: "899" },
-        { id: "7", title: "Wireless", review: "⭐⭐⭐", img: "img/we3.jpg", price: "699" },
-        { id: "8", title: "SmartWatch", review: "⭐⭐", img: "img/ww3.png", price: "1999" },
-        { id: "9", title: "EarPhone", review: "⭐⭐⭐", img: "img/e4.jpg", price: "199" },
-        { id: "10", title: "HeadPhone", review: "⭐⭐⭐⭐", img: "img/h4.jpg", price: "1099" },
-        { id: "11", title: "Wireless", review: "⭐⭐", img: "img/we4.jpg", price: "799" },
-        { id: "12", title: "SmartWatch", review: "⭐⭐⭐", img: "img/ww4.jpg", price: "1299" },
-        { id: "13", title: "EarPhone", review: "⭐⭐⭐", img: "img/e5.jpg", price: "329" },
-        { id: "14", title: "HeadPhone", review: "⭐⭐⭐⭐", img: "img/h5.jpg", price: "749" },
-        { id: "15", title: "Wireless", review: "⭐⭐⭐⭐", img: "img/we5.jpg", price: "659" },
-        { id: "16", title: "SmartWatch", review: "⭐⭐⭐⭐", img: "img/ww5.jpg", price: "999" },
-    ];
+  return (
+    <>
+      <section>
+        <div className="   ">
+          <div className="font-bold text-4xl flex  justify-center py-3 moreSmall:ml-4">
+            Shop by Category
+          </div>
+          {shop && (
+            <div className="flex justify-center flex-col">
+              <div className=" sm:overflow-x-scroll ssm:overflow-x-scroll">
+                <ul className="flex gap-3 justify-start px-5 m-0 ">
+                  <li
+                    className="border border-violet-600 bg-violet-400 rounded-md px-2 py-1  font-semibold cursor-pointer "
+                    onClick={() => filterEarphone("All")}
+                  >
+                    All{" "}
+                  </li>
+                  <li
+                    className="border border-violet-600 bg-violet-400 rounded-md px-2 py-1 font-semibold cursor-pointer "
+                    onClick={() => filterEarphone("EarPhone")}
+                  >
+                    {" "}
+                    EarPhone{" "}
+                  </li>
+                  <li
+                    className="border border-violet-600 bg-violet-400 rounded-md px-2 py-1  font-semibold cursor-pointer "
+                    onClick={() => filterEarphone("HeadPhone")}
+                  >
+                    {" "}
+                    HeadPhone{" "}
+                  </li>
+                  <li
+                    className="border border-violet-600 bg-violet-400 rounded-md px-2 py-1  font-semibold cursor-pointer "
+                    onClick={() => filterEarphone("Wireless")}
+                  >
+                    {" "}
+                    Wireless{" "}
+                  </li>
+                  <li
+                    className="border border-violet-600 bg-violet-400 rounded-md px-2  py-1 font-semibold cursor-pointer"
+                    onClick={() => filterEarphone("SmartWatch")}
+                  >
+                    {" "}
+                    SmartWatch{" "}
+                  </li>
+                </ul>
+              </div>
+              <div
+                className={
+                  " grid lg:grid-cols-3 xl:grid-cols-4   gap-2   rounded-lg  shadow-2xl  md:grid-cols-2 sm:grid-cols-1 ssm:grid-cols-1 middle:grid-cols-3   p-5"
+                }
+              >
+                {filteredData.map((pro) => (
+                  <ShopList
+                    key={pro.id}
+                    id={pro.id}
+                    product={pro}
+                    shop={shop}
+                    AddtoCart={AddtoCart}
+                    handleremove={handleremove}
+                    cart={cart}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
 
-
-    const filterEarphone=(category)=>{
-        setFilter(category)
-        console.log(filter)
-        console.log(productData)
-
-    }
-    const filteredData= productData.filter(product=> filter==='All' || product.title===filter);
-
-    return (
-
-        <>
-            <section>
-                <div className='   '>
-                    <div className='font-bold text-4xl flex  justify-center py-3 moreSmall:ml-4'>
-                        Shop by Category
-                    </div>
-                    {shop && (
-                        <div className='flex justify-center flex-col'>
-
-                             <div className=' sm:overflow-x-scroll ssm:overflow-x-scroll' >
-                        <ul className='flex gap-3 justify-start px-5 m-0 '>
-                            <li className='border border-violet-600 bg-violet-400 rounded-md px-2 py-1  font-semibold cursor-pointer ' onClick={()=>filterEarphone('All')} >All </li>
-                            <li className='border border-violet-600 bg-violet-400 rounded-md px-2 py-1 font-semibold cursor-pointer ' onClick={()=>filterEarphone('EarPhone')}> EarPhone </li>
-                            <li className='border border-violet-600 bg-violet-400 rounded-md px-2 py-1  font-semibold cursor-pointer ' onClick={()=>filterEarphone('HeadPhone')} > HeadPhone </li>
-                            <li className='border border-violet-600 bg-violet-400 rounded-md px-2 py-1  font-semibold cursor-pointer ' onClick={()=>filterEarphone('Wireless')} > Wireless </li>
-                            <li className='border border-violet-600 bg-violet-400 rounded-md px-2  py-1 font-semibold cursor-pointer' onClick={()=>filterEarphone('SmartWatch')} > SmartWatch </li>
-                        </ul>
-                    </div>
-                            <div className={" grid lg:grid-cols-3 xl:grid-cols-4   gap-2   rounded-lg  shadow-2xl  md:grid-cols-2 sm:grid-cols-1 ssm:grid-cols-1 middle:grid-cols-3   p-5"}>
-
-                                {filteredData.map((pro) => (
-                                    <ShopList
-                                        key={pro.id}
-                                        id={pro.id}
-                                        product={pro}
-                                        shop={shop}
-                                        AddtoCart={AddtoCart}
-                                        handleremove={handleremove}
-                                        cart={cart}
-                                    />
-                                ))}
-
-                            </div>
-                        </div>
-                    )}
-
-                    {!shop && (
-                        <div className=''>
-                            <div className={"  "}>
-                                {productData.slice(0, 1).map((pro) => (
-                                    <ShopList
-                                        key={pro.id}
-                                        id={pro.id}
-                                        product={pro}
-                                        shop={shop}
-                                        AddtoCart={AddtoCart}
-                                        handleremove={handleremove}
-                                        cart={cart}
-                                    />
-                                ))}
-
-                            </div>
-                        </div>
-
-                    )}
-                    <Cart cartitem={cart} removeItem={removeItem} handleremove={handleremove} cart={cart} quantity={quantity} setquantity={setquantity} />
-                </div>
-                <div className='flex  justify-center'>
-                    <button className='border border-violet-500 text-cyan-100 font-bold w-40 h-10 rounded-full bg-violet-400 items-center mt-20 text-xl hover:bg-violet-500' onClick={handleshop}> {shop ? "Show Less" : "Show All"}  </button>
-                  
-                </div>
-            </section>
-        </>
-    )
-}
-export default Shopping
-
+          {!shop && (
+            <div className="">
+              <div className={"  "}>
+                {productData.slice(0, 1).map((pro) => (
+                  <ShopList
+                    key={pro.id}
+                    id={pro.id}
+                    product={pro}
+                    shop={shop}
+                    AddtoCart={AddtoCart}
+                    handleremove={handleremove}
+                    cart={cart}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+          <Cart
+            cartitem={cart}
+            removeItem={removeItem}
+            handleremove={handleremove}
+            cart={cart}
+            quantity={quantity}
+            setquantity={setquantity}
+          />
+        </div>
+        <div className="flex  justify-center">
+          <button
+            className="border border-violet-500 text-cyan-100 font-bold w-40 h-10 rounded-full bg-violet-400 items-center mt-20 text-xl hover:bg-violet-500"
+            onClick={handleshop}
+          >
+            {" "}
+            {shop ? "Show Less" : "Show All"}{" "}
+          </button>
+        </div>
+      </section>
+      {/* <Home handleshop={handleshop} /> */}
+    </>
+  );
+};
+export default Shopping;
