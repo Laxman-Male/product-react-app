526b3469e63e73b5bb76eafd316827e08137578d
Laxman-Male <laxmanmale122@gmail.com>
2025-01-05 00:13:47 +0530

    chor:-removed navbar from website

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,111 +1,143 @@
-import React, { useEffect, useState } from 'react'
-import Shoppingg from './Shopping'
-import ShopList from './ShopList'
+import React, { useEffect, useState } from "react";
+import Shoppingg from "./Shopping";
+import ShopList from "./ShopList";
 
 const Cart = ({ setquantity, quantity, handleremove, cartitem = [] }) => {
- 
- const [displayCart, setdisplayCart]=useState(false)
+  const [displayCart, setdisplayCart] = useState(false);
   const [total, settotal] = useState(0);
- 
+
   useEffect(() => {
     let newtotal = 0;
-    cartitem.forEach(item => {
+    cartitem.forEach((item) => {
       const itemQuntity = quantity[item.id] || 1;
       newtotal = newtotal + parseFloat(item.price) * itemQuntity;
     });
     settotal(newtotal);
-    
-  }, [cartitem, quantity])
-  
-useEffect(()=>{
-  if(total>0){
-    setcount(prevCount=>prevCount+1)
-  }
- 
-},[])
+  }, [cartitem, quantity]);
+
+  useEffect(() => {
+    if (total > 0) {
+      setcount((prevCount) => prevCount + 1);
+    }
+  }, []);
 
   const handleplus = (item) => {
-
-    setquantity(
-      (quantity) => {
-        const currentQuantity = quantity[item.id] || 1;
-        return {
-          ...quantity,
-          [item.id]: currentQuantity + 1,
-
-        }
-      });
+    setquantity((quantity) => {
+      const currentQuantity = quantity[item.id] || 1;
+      return {
+        ...quantity,
+        [item.id]: currentQuantity + 1,
+      };
+    });
   };
   const handleminus = (item) => {
-    setquantity(
-      (quantity) => {
-        const currentQuantity = quantity[item.id] || 1;
-        if (currentQuantity > 0) {
-          return {
-            ...quantity,
-            [item.id]: currentQuantity - 1,
-          }
-        }
-        else {
-          return quantity;
-
-        }
-      });
-  }
+    setquantity((quantity) => {
+      const currentQuantity = quantity[item.id] || 1;
+      if (currentQuantity > 0) {
+        return {
+          ...quantity,
+          [item.id]: currentQuantity - 1,
+        };
+      } else {
+        return quantity;
+      }
+    });
+  };
 
   const handleCartDesign = () => {
-    console.log("enter in cart")
-    setdisplayCart(!displayCart)
-  }
-  const continue_shop=()=>{
-    setdisplayCart(!displayCart)
-  }
+    console.log("enter in cart");
+    setdisplayCart(!displayCart);
+  };
+  const continue_shop = () => {
+    setdisplayCart(!displayCart);
+  };
 
   return (
     <>
-
-      <ul  className='absolute top-16 right-2 max-h-[80vh] overflow-y-auto  '>
-        <li className='cursor-pointer' onClick={handleCartDesign}><lord-icon
-          src="https://cdn.lordicon.com/pbrgppbb.json"
-          trigger="hover">
-         </lord-icon> </li>
+      <ul className="absolute top-2 right-2 max-h-[80vh] overflow-y-auto  ">
+        <li className="cursor-pointer" onClick={handleCartDesign}>
+          <lord-icon
+            src="https://cdn.lordicon.com/pbrgppbb.json"
+            trigger="hover"
+          ></lord-icon>{" "}
+        </li>
       </ul>
-         <li className={`absolute top-16 list-none right-1 rounded-full w-4 h-4 flex  justify-center items-center text-black bg-red-600  ${total==0? 'hidden': ''  } ` }>  </li>
+      <li
+        className={`absolute top-16 list-none right-1 rounded-full w-4 h-4 flex  justify-center items-center text-black bg-red-600  ${
+          total == 0 ? "hidden" : ""
+        } `}
+      >
+        {" "}
+      </li>
 
-      <div className={`fixed top-12 right-1 max-h-[80vh] overflow-y-auto  border border-violet-400 w-3/4  bg-violet-300  rounded-xl ${ displayCart? 'block': "hidden" }`}>
-        <div className='flex   justify-center '>
-
-           <div className='  grid lg:grid-cols-4 xl:grid-cols-4      rounded-lg  shadow-2xl  md:grid-cols-2 sm:grid-cols-2  '>
-
+      <div
+        className={`fixed top-12 right-1 max-h-[80vh] overflow-y-auto  border border-violet-400 w-3/4  bg-violet-300  rounded-xl ${
+          displayCart ? "block" : "hidden"
+        }`}
+      >
+        <div className="flex   justify-center ">
+          <div className="  grid lg:grid-cols-4 xl:grid-cols-4      rounded-lg  shadow-2xl  md:grid-cols-2 sm:grid-cols-2  ">
             {cartitem.map((item) => (
-              <div key={item.id}  >
-                 <img className='w-3/4 h-44  m-2 items-center shadow-xl shadow-violet-700/80  rounded-tr-3xl rounded-bl-3xl ' src={item.img} />
-                <p className=' px-2 font-bold text-lg ml-1'>{item.title}</p>
-                <p className='px-3 font-semibold text-xl'>Rs.{item.price}</p>
-                <div className=' flex items-center   ml-3 '  >
-                  <button className='border border-violet-700 font-bold w-8 rounded-l-md' onClick={() => handleminus(item)}>  - </button>
-                  <p className='border border-violet-700 px-2  w-8 m-0 ' > {quantity[item.id] || 1} </p>
-                  <button className='border border-violet-700 font-bold w-8 rounded-r-md' onClick={() => handleplus(item)}>  + </button>
+              <div key={item.id}>
+                <img
+                  className="w-3/4 h-44  m-2 items-center shadow-xl shadow-violet-700/80  rounded-tr-3xl rounded-bl-3xl "
+                  src={item.img}
+                />
+                <p className=" px-2 font-bold text-lg ml-1">{item.title}</p>
+                <p className="px-3 font-semibold text-xl">Rs.{item.price}</p>
+                <div className=" flex items-center   ml-3 ">
+                  <button
+                    className="border border-violet-700 font-bold w-8 rounded-l-md"
+                    onClick={() => handleminus(item)}
+                  >
+                    {" "}
+                    -{" "}
+                  </button>
+                  <p className="border border-violet-700 px-2  w-8 m-0 ">
+                    {" "}
+                    {quantity[item.id] || 1}{" "}
+                  </p>
+                  <button
+                    className="border border-violet-700 font-bold w-8 rounded-r-md"
+                    onClick={() => handleplus(item)}
+                  >
+                    {" "}
+                    +{" "}
+                  </button>
                 </div>
-                 <button className=' border border-violet-500 rounded-xl py-1 px-4 my-1 text-lg font-semibold ml-3   ' onClick={() => handleremove(item.id)}> Remove  </button>
-               </div>
+                <button
+                  className=" border border-violet-500 rounded-xl py-1 px-4 my-1 text-lg font-semibold ml-3   "
+                  onClick={() => handleremove(item.id)}
+                >
+                  {" "}
+                  Remove{" "}
+                </button>
+              </div>
             ))}
           </div>
-
         </div>
-        <div className='flex justify-center'>
-          <p className=' cursor-pointer p-2 text-blue-700 font-normal underline' onClick={continue_shop}>  Continue Shopping  </p>
-
+        <div className="flex justify-center">
+          <p
+            className=" cursor-pointer p-2 text-blue-700 font-normal underline"
+            onClick={continue_shop}
+          >
+            {" "}
+            Continue Shopping{" "}
+          </p>
         </div>
-        <div className=' border border-violet-400 rounded-lg cursor-pointer  self-center p-3 bg-gradient-to-b from-violet-500 to-violet-200 text-md font-semibold   text-center'>
-          <p className={`' font-bold font-sans text-lg'${total==0? "hidden": "block"}`}> Total: Rs.{total}</p>
+        <div className=" border border-violet-400 rounded-lg cursor-pointer  self-center p-3 bg-gradient-to-b from-violet-500 to-violet-200 text-md font-semibold   text-center">
+          <p
+            className={`' font-bold font-sans text-lg'${
+              total == 0 ? "hidden" : "block"
+            }`}
+          >
+            {" "}
+            Total: Rs.{total}
+          </p>
         </div>
-       </div>
-
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
-
- 
+export default Cart;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,157 +1,146 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import Cart from './Cart'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
-import { faUserNurse } from '@fortawesome/free-solid-svg-icons';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { Offcanvas, Button, Dropdown } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useForm, Controller } from "react-hook-form"
-import { height, width } from '@fortawesome/free-solid-svg-icons/fa0';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Cart from "./Cart";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faUserNurse } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { Offcanvas, Button, Dropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useForm, Controller } from "react-hook-form";
+import { height, width } from "@fortawesome/free-solid-svg-icons/fa0";
 
 const Navbar = () => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: "",
+      select: {},
+    },
+  });
+  const onSubmit = (data) => console.log(data);
 
-    const { control, handleSubmit } = useForm({
-        defaultValues: {
-            firstName: "",
-            select: {},
-        },
-    })
-    const onSubmit = (data) => console.log(data)
+  const [showSetting, setShowSetting] = useState(false);
+  const [showAnother, setShowAnother] = useState(false);
 
-const [showSetting, setShowSetting] = useState(false);
-const [showAnother, setShowAnother] = useState(false);
-
-// Toggle functions for each offcanvas
-const handleShowSetting = () => {
+  // Toggle functions for each offcanvas
+  const handleShowSetting = () => {
     setShowSetting(true);
-    console.log('show another')
-}
-const handleCloseSetting = () => setShowSetting(false);
+    console.log("show another");
+  };
+  const handleCloseSetting = () => setShowSetting(false);
 
-const handleShowAnother = () => {
+  const handleShowAnother = () => {
     setShowAnother(true);
-    console.log("another show ")
-}
-const handleCloseAnother = () => setShowAnother(false);
+    console.log("another show ");
+  };
+  const handleCloseAnother = () => setShowAnother(false);
 
+  const [screenSize, setscreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerWidth,
+  });
 
-
-
-    const [screenSize, setscreenSize] = useState({
+  useEffect(() => {
+    const handleResize = () => {
+      setscreenSize({
         width: window.innerWidth,
         height: window.innerWidth,
-    });
+      });
+    };
 
-    useEffect(() => {
-        const handleResize = () => {
-            setscreenSize({
-                width: window.innerWidth,
-                height: window.innerWidth,
-            })
-        }
+    window.addEventListener("resize", handleResize);
 
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-        window.addEventListener('resize', handleResize)
+  return (
+    <>
+      <div className=" m-2">
+        <Button variant="outline-secondary" onClick={handleShowAnother}>
+          <img
+            src="img/setting.png"
+            className="w-6 mix-blend-darken moreSmall:w-5 ssm:w-5  sm:w-4 md:w-5 lg:w-6  "
+          />
+        </Button>
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, [])
-
-    return (
-        <>
-            
-            <div className={`flex  `} >
-
-                <div className=' bg-violet-50 w-full '>
-                    <div  >
- 
-                    </div>
-                    <ul className=' flex gap-5 justify-center px-5 py-2'>
-
-                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'>
-
-                            <FontAwesomeIcon icon={faHouse} />
-                        </li>
-                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'>
-                            <FontAwesomeIcon icon={faGlobe} />
-                        </li>
-                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'>
-                            <FontAwesomeIcon icon={faUserNurse} />
-                        </li>
-                        <li className=' font-semibold hover:font-bold cursor-pointer text-xl'>
-                            <FontAwesomeIcon icon={faPhone} />
-                        </li>
-                    </ul>
-                </div>
+        <Offcanvas
+          show={showAnother}
+          onHide={handleCloseAnother}
+          placement="start"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Setting </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <div className="p-1 cursor-pointer border border-gray-500 rounded-xl items-center flex gap-3">
+              <FontAwesomeIcon
+                icon={faUser}
+                className="border border-gray-600 p-1 rounded-xl"
+              />{" "}
+              View your Profile
             </div>
 
-            <div className=''>
+            <form className=" text-lg  p-2">
+              <div>
+                <input type="checkbox" />
+                <label className="px-2"> EarPhone</label>
+              </div>
+              <div>
+                <input type="checkbox" />
+                <label className="px-2"> HeadPhone</label>
+              </div>
+              <div>
+                <input type="checkbox" />
+                <label className="px-2"> SmartWatch </label>
+              </div>
+              <div>
+                <input type="checkbox" />
+                <label className="px-2">Wireless </label>
+              </div>
+            </form>
+          </Offcanvas.Body>
+          <div>
+            <p className="font-semibold m-0 "> Contact us at </p>
+            <ul className="flex gap-4">
+              <li>
+                <img
+                  className="w-5 cursor-pointer"
+                  src="https://tse1.mm.bing.net/th/id/OIP.Rwdx3-MJLtAa17YZIFMunAAAAA?rs=1&pid=ImgDetMain"
+                />
+              </li>
+              <li>
+                <img
+                  className="w-5 cursor-pointer"
+                  src="https://image.similarpng.com/very-thumbnail/2020/06/Black-icon-Instagram-logo-transparent-PNG.png"
+                />
+              </li>
+              <li>
+                <img
+                  className="w-5 cursor-pointer"
+                  src="https://th.bing.com/th/id/R.8f265a94a8fd9b0cb24488678ab6d648?rik=79r6hq6icjgjRg&riu=http%3a%2f%2flofrev.net%2fwp-content%2fphotos%2f2016%2f07%2fyoutube_black_logo.jpg&ehk=PyW73Rx8P9vyochnjVRFEEH0HO8s8Xzgt7XVUA3igrE%3d&risl=&pid=ImgRaw&r=0"
+                />
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/laxman-male-0716b62a1/"
+                  target="blank"
+                >
+                  <img
+                    className="w-5 cursor-pointer"
+                    src="https://tse3.mm.bing.net/th/id/OIP.DtzCFTem9bd_3GlVROBlRgHaHa?rs=1&pid=ImgDetMain"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Offcanvas>
+      </div>
+    </>
+  );
+};
 
-<Button variant='outline-secondary' onClick={handleShowAnother}>
-                    <img src='img/setting.png' className='w-6 mix-blend-darken moreSmall:w-5 ssm:w-5  sm:w-4 md:w-5 lg:w-6  ' />
-                </Button>
-
-                <Offcanvas show={showAnother} onHide={handleCloseAnother} placement="start">
-                    <Offcanvas.Header closeButton>
-                        <Offcanvas.Title >Setting </Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body >
-                        <div className='p-1 cursor-pointer border border-gray-500 rounded-xl items-center flex gap-3'>
-                            <FontAwesomeIcon icon={faUser} className='border border-gray-600 p-1 rounded-xl' />     View your Profile
-                        </div>
-
-                        <form className=' text-lg  p-2'>
-                            <div>
-                                <input type='checkbox' />
-                                <label className='px-2'> EarPhone</label>
-                            </div>
-                            <div>
-                                <input type='checkbox' />
-                                <label className='px-2'> HeadPhone</label>
-                            </div>
-                            <div>
-                                <input type='checkbox' />
-                                <label className='px-2'> SmartWatch  </label>
-                            </div>
-                            <div>
-                                <input type='checkbox' />
-                                <label className='px-2'>Wireless </label>
-                            </div>
-                        </form>
-                    </Offcanvas.Body>
-                    <div>
-                        <p className='font-semibold m-0 '> Contact us at </p>
-                        <ul className='flex gap-4'>
-                            <li>
-                                <img className='w-5 cursor-pointer' src='https://tse1.mm.bing.net/th/id/OIP.Rwdx3-MJLtAa17YZIFMunAAAAA?rs=1&pid=ImgDetMain' />
-                            </li>
-                            <li>
-                                <img className='w-5 cursor-pointer' src='https://image.similarpng.com/very-thumbnail/2020/06/Black-icon-Instagram-logo-transparent-PNG.png' />
-                            </li>
-                            <li>
-                                <img className='w-5 cursor-pointer' src='https://th.bing.com/th/id/R.8f265a94a8fd9b0cb24488678ab6d648?rik=79r6hq6icjgjRg&riu=http%3a%2f%2flofrev.net%2fwp-content%2fphotos%2f2016%2f07%2fyoutube_black_logo.jpg&ehk=PyW73Rx8P9vyochnjVRFEEH0HO8s8Xzgt7XVUA3igrE%3d&risl=&pid=ImgRaw&r=0' />
-                            </li>
-                            <li>
-                                <a href='https://www.linkedin.com/in/laxman-male-0716b62a1/' target='blank'>
-                                    <img className='w-5 cursor-pointer' src='https://tse3.mm.bing.net/th/id/OIP.DtzCFTem9bd_3GlVROBlRgHaHa?rs=1&pid=ImgDetMain' />
-                                </a>
-                            </li>
-                        </ul>
-
-                    </div>
-                </Offcanvas>
-</div>
-
-
-
-
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
